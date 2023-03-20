b7f61edf7d0389943a2c346b37c22a0d25663aae
Joel Cheah <gjoelcheah@gmail.com>
2023-03-20 02:12:36 +0000

    Moved js files in lib folder

---
+++ src/lib/routelist.js
@@ -0,0 +1,20 @@
+export const ROUTES = Object.freeze({
+	root: '/',
+	// Authentication
+	login: '/auth/login',
+	signup: '/auth/signup',
+	forgotPassword: '/forgot-password',
+
+	//Student
+	feed: '/student/feed',
+	discover: '/student/discover',
+	profile: '/student/profile',
+	editProfile: '/student/profile/edit',
+
+	//Organization
+	requestOrgAccess: '/request-organization-access',
+	orgDashboard: '/organization',
+
+	//Admin
+	adminDashboard: '/admin'
+});
