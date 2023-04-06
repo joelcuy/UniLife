82205f936104008230031d34e3dc912f5379d701
Joel Cheah <gjoelcheah@gmail.com>
2023-04-06 01:11:24 +0100

    import custom.scss from root layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script>
+	import '../styles/custom.scss';
 	import { signOut, onAuthStateChanged } from 'firebase/auth';
 	import { goto } from '$app/navigation';
 	import { auth } from '$lib/Firebase';
