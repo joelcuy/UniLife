2cbfec8130ffcfbca0d7fc4b2c5e206b21fc72e8
Joel Cheah <gjoelcheah@gmail.com>
2023-04-12 08:45:22 +0100

    Test student feed

--- src/routes/student/feed/+page.svelte
+++ src/routes/student/feed/+page.svelte
@@ -1,11 +1,61 @@
 <script>
-	import { signOut, onAuthStateChanged } from 'firebase/auth';
-	import { goto } from '$app/navigation';
-	import { auth } from '$lib/Firebase';
-	import { Form, FormGroup, Input, Label, Button, Spinner, Alert } from 'sveltestrap';
-	import { onMount } from 'svelte';
-	import { ROUTES } from '$lib/routelist';
-	import { signout } from '$lib/auth.js';
+	import Post from './Post.svelte';
+	import { auth, app, db } from '$lib/Firebase';
+	import { onMount, afterUpdate, onDestroy } from 'svelte';
+	import {
+		getFirestore,
+		collection,
+		query,
+		where,
+		getDocs,
+		doc,
+		setDoc,
+		getDoc,
+		onSnapshot
+	} from 'firebase/firestore';
+	import CenteredSpinner from '../../../lib/components/general/CenteredSpinner.svelte';
+
+	let isLoading = true;
+	let ecaPosts = [];
+	onMount(async () => {
+		const ecaPostRef = collection(db, 'ecaPosts');
+
+		const ecaPostSnapshot = await getDocs(ecaPostRef);
+
+		// Create an array of promises for each document in the snapshot
+		const promises = ecaPostSnapshot.docs.map(async (postDoc) => {
+			const orgUID = postDoc.data().orgUidFk;
+			const orgRef = doc(db, 'users', orgUID);
+			const orgSnapshot = await getDoc(orgRef);
+			// console.log(orgSnapshot.data());
+
+			return { ...postDoc.data(), uid: postDoc.id, organizer: orgSnapshot.data() };
+		});
+
+		// Wait for all promises to resolve
+		ecaPosts = await Promise.all(promises);
+
+		console.log(ecaPosts);
+		isLoading = false;
+
+		// ecaPostSnapshot.forEach(async (postDoc) => {
+		// 	const orgUID = postDoc.data().orgUidFk;
+		// 	const orgRef = doc(db, 'users', orgUID);
+		// 	const orgSnapshot = await getDoc(orgRef);
+		// 	// console.log(orgSnapshot.data());
+
+		// 	ecaPosts.push({ ...postDoc.data(), uid: postDoc.id, organizer: orgSnapshot.data() });
+		// });
+
+		// console.log(ecaPosts);
+		// isLoading = false;
+	});
 </script>
 
-<h4>Student Home</h4>
+{#if isLoading}
+	<CenteredSpinner />
+{:else}
+	{#each ecaPosts as post}
+		<Post {post} />
+	{/each}
+{/if}
---
+++ src/routes/student/feed/Post.svelte
@@ -0,0 +1,64 @@
+<script>
+	import { Carousel, CarouselControl, CarouselIndicators, CarouselItem } from 'sveltestrap';
+
+	export let post;
+	let activeIndex = 0;
+</script>
+
+<div class="post">
+	<div class="user-info">
+		<img src={post.organizer.profilePic.URL} alt="{post.organizer.orgName}'s avatar" />
+		<span>{post.organizer.orgName}</span>
+	</div>
+	<Carousel items={post.images} bind:activeIndex>
+		<CarouselIndicators class="" bind:activeIndex items={post.images} />
+
+		<div class="carousel-inner">
+			{#each post.images as image, index}
+				<CarouselItem bind:activeIndex itemIndex={index}>
+					<img src={image.URL} class="d-block w-100" alt={`${image} ${index + 1}`} />
+				</CarouselItem>
+			{/each}
+		</div>
+
+		<CarouselControl direction="prev" bind:activeIndex items={post.images} />
+		<CarouselControl direction="next" bind:activeIndex items={post.images} />
+	</Carousel>
+	<!-- {#each post.images as image}
+		<img class="post-image" src={image.URL} alt="Post" />
+	{/each} -->
+	<!-- <div class="post-actions">
+		<span>{post.likes} likes</span>
+		You can add more actions here, like buttons for like, comment, and share
+	</div> -->
+	<div class="post-description">
+		<p>{post.description}</p>
+	</div>
+</div>
+
+<style>
+	.post {
+		margin-bottom: 24px;
+	}
+	.user-info {
+		display: flex;
+		align-items: center;
+		padding: 8px;
+	}
+	.user-info img {
+		width: 32px;
+		height: 32px;
+		border-radius: 50%;
+		margin-right: 8px;
+	}
+	.post-image {
+		width: 100%;
+		height: auto;
+	}
+	.post-actions {
+		padding: 8px;
+	}
+	.post-description {
+		padding: 8px;
+	}
+</style>
