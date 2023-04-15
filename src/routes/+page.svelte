19f935590e53aa5ccbc67ad7f0e59c2127a12f2e
Joel Cheah <gjoelcheah@gmail.com>
2023-04-15 18:28:13 +0100

    Redirect root to login

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { goto } from "$app/navigation";
+	import { onMount } from "svelte";
+    import { ROUTES } from "../lib/routelist";
+
+    // No landing page, so redirect to login
+    onMount(()=>{
+        goto(ROUTES.login);
+    })
+</script>
