6cdbe8cdf334927ca867866ec42dd7d557671ac2
Joel Cheah <gjoelcheah@gmail.com>
2023-04-15 20:47:23 +0100

    Converting to PWA

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		fs: {
+			allow: ['..'],
+		},
+	},
+	// Add any other Vite configurations you already have here
 });
